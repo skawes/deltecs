2699cd5641028453c6cc345d9611179f5d0f896b
skawes <36124620+skawes@users.noreply.github.com>
2019-07-09 18:26:06 +0530

    Update todo.js

--- TO DO APP/js/todo.js
+++ TO DO APP/js/todo.js
@@ -1,13 +1,47 @@
-var a=document.querySelector(".menu");
-a.addEventListener("click",function(){
-   // this.classList.add.('des');
-  var el=event.target.id;
- 
-  
-//  document.querySelector("#"+el).classList.add('des');
- 
-    var cl=document.querySelectorAll(".workspace");
-     for(var i=0;i<cl.length;i++)
-       cl[i].classList.add('hide');
-    document.querySelector("."+el).classList.remove('hide');
+$(document).ready(function () {
+  $(".add-task").click(function () {
+    $(".action").text("Add tasks");
+    $(".add-task,.menu,.workspace,.fas").hide();
+    $(".add-space,.icon").show();
+    // $('.fa-check-circle').replaceWith("");
+  });
+});
+
+$(document).ready(function () {
+  $('.icon').click(function(){
+    $(".action").text("All the tasks");
+    $(".add-space,.icon").hide();
+    $(".menu,.all,.fas").show();
+  })
+
+});
+/*
+ var a = document.querySelector(".menu");
+a.addEventListener("click", function () {
+  // this.classList.add.('des');
+  var el = event.target.id;
+  if (el == 'all' || el == 'pending' || el == 'completed') {
+    var co = document.querySelectorAll(".menu-item");
+    for (var i = 0; i < co.length; i++)
+      co[i].classList.remove('des');
+    document.querySelector("#" + el).classList.add('des');
+  }
+  if (el == 'all' || el == 'pending' || el == 'completed') {
+    var cl = document.querySelectorAll(".workspace");
+    for (var i = 0; i < cl.length; i++)
+      cl[i].classList.add('hide');
+    document.querySelector("." + el).classList.remove('hide');
+  }
+}); 
+ */
+
+$(document).ready(function () {
+$(".menu-item").click(function(){
+  $(".workspace").addClass('hide');
+  $this=$(this);
+  $('.workspace[data-navname=' + $this.data('navname') + ']').removeClass('hide');
+  $(".menu-item").removeClass('des');
+  $('.menu-item[data-navname=' + $this.data('navname') + ']').addClass('des');
+});
+
 });
